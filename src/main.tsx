fa9324d9ad1f04871b39f56912d1aec92911b7cc
foxigaoqian <foxigaoqian@gmail.com>
2026-07-22 17:01:19 +0800

    Mount the React app with restored third-party API configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import ApiConfiguredApp from './ApiConfiguredApp.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApiConfiguredApp />
   </StrictMode>,
 );
